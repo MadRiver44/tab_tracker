c98101f0c013b5169c1b39b4e0b49f8d15cfb01f
Kevin Turney <super996@optonline.net>
2020-06-12 13:05:27 -0400

    server setup and middleware

---
+++ tab_tracker_server/src/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+const port = 3000
+
+app.get('/status', (req, res) => res.send({ message: 'The start of a Turney Heavy Industries' }))
+
+app.listen(port, () => console.log(`THI is listening on ${port}`))
