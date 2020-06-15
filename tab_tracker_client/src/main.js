860719906be9305b5839248338591683eeaa17ae
Kevin Turney <super996@optonline.net>
2020-06-15 13:44:39 -0400

    refresh and style Registration page :lipstick:

--- tab_tracker_client/src/main.js
+++ tab_tracker_client/src/main.js
@@ -1,13 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
-
+Vue.use(Vuetify)
 new Vue({
   router,
   store,
+  vuetify: new Vuetify(),
   render: h => h(App)
 }).$mount('#app')
--- tab_tracker_client/src/router/index.js
+++ tab_tracker_client/src/router/index.js
@@ -1,29 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../pages/Home.vue'
 import Register from '../pages/Register.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
     path: '/register',
     name: 'Register',
     component: Register
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../pages/About.vue')
   }
 ]
 
